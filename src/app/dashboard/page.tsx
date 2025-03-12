3383feba51c6076dbc391bf09aa6f6a7aca4769a
= <rohansinglawork@gmail.com>
2025-03-12 22:39:07 +0530

    chart display

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,38 +4,38 @@ import PriceOverview from "./components/PriceOverview";
 import MarketMovers from "./components/MarketMovers";
 import TopGainers from "../components/TopGainers";
 import TopLosers from "../components/TopLosers";
+import AptosChart from "../components/AptosChart";
 
 export default function DashboardPage() {
   return (
     <>
-    
-    <div className="flex w-full justify-between">
-    <TopGainers/>
-    <TopLosers/>
-  
-    </div>
-  
-  <div className="grid grid-cols-1 lg:grid-cols-12 gap-6 p-6">
-      {/* Price Overview Section */}
-      <div className="lg:col-span-8">
-        <PriceOverview />
+<AptosChart/>
+      <div className="flex w-full justify-between">
+        <TopGainers />
+        <TopLosers />
       </div>
 
-      {/* News Feed Section */}
-      <div className="lg:col-span-4">
-        <NewsSection />
-      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-6 p-6">
+        {/* Price Overview Section */}
+        <div className="lg:col-span-8">
+          <PriceOverview />
+        </div>
 
-      {/* Top Dapps Section */}
-      <div className="lg:col-span-6">
-        <TopDapps />
-      </div>
+        {/* News Feed Section */}
+        <div className="lg:col-span-4">
+          <NewsSection />
+        </div>
 
-      {/* Market Movers Section */}
-      <div className="lg:col-span-6">
-        <MarketMovers />
+        {/* Top Dapps Section */}
+        <div className="lg:col-span-6">
+          <TopDapps />
+        </div>
+
+        {/* Market Movers Section */}
+        <div className="lg:col-span-6">
+          <MarketMovers />
+        </div>
       </div>
-    </div>
     </>
   );
 }
